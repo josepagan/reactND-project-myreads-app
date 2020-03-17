690b4861ecce14b0c6c888e9d86575562a845bc0
Jose Pagan <jm.pagan@gmail.com>
2020-03-17 13:29:13 +0000

    style:deleting unused code

--- src/App.js
+++ src/App.js
@@ -7,38 +7,17 @@ import ListBooks from "./components/ListBooks";
 
 const App = () => {
   const [showSearchPage, setShowSearchPage] = useState(false);
-  //books in
   const [books, setBooks] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [mergedBooks, setMergedBooks] = useState([]);
 
   //i think i have to divide the 3 operations, change shelf, 
   const changeShelf = (book, shelf) => {
-//first creating a shallow copy
-    const updatedBooks = [...books];
-    
-    //lets find the object in state the state, if not there lets push it there
-    let found = updatedBooks.findIndex(element => book.id === element.id);
-    if (found === -1) {
-      updatedBooks.push(book);
-      found = books.length - 1;
-      console.log("PFOUND",found)
-    }
-    //add the necesary properties
-
-    updatedBooks[found].shelf = shelf;
-    updatedBooks[found].stamp = Date.now();
-
-    //update server (sever only cares of book.id and the shelf)
-    //lets confirm that server is ok and then call setBooks
     BooksAPI.update(book, shelf).then(data => {
       console.log(data);
       BooksAPI.getAll().then(data => {
         setBooks(data);
       });
-  
-      // if (data[shelf].includes(book.id)) 
-      // setBooks(updatedBooks);
     });
   };
 
@@ -90,7 +69,5 @@ const App = () => {
     </BrowserRouter>
   );
 };
-
-
 
 export default App;
